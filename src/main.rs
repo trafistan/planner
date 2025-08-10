9032075d1051864d839b219d67801bae80b4da3f
Ersoy Filinte <ersoy.filinte@gmail.com>
2025-08-11 01:57:10 +0300

    Update `Cargo.lock` with newly added dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, Rust!");
+use std::env;
+use std::error::Error;
+use postgres::{Client, NoTls};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut client = Client::connect(&format!(
+        "host=tf-2025081018303032650000000a.cluster-c1y2k8e8uxdb.eu-central-1.rds.amazonaws.com user=postgres password={} dbname=db",
+        env::var("PASSWORD")?
+    ), NoTls)?;
+
+    for row in client.query("SELECT id, url, sitemap FROM websites", &[])? {
+        let id: i16 = row.get(0);
+        let url: &str = row.get(1);
+        let sitemap: &str = row.get(2);
+
+        println!("found website: {} {} {:?}", id, url, sitemap);
+    }
+
+    Ok(())
 }
