9e065994fd7ddd9147f28273e6b1cf627480ef16
Ersoy Filinte <ersoy.filinte@gmail.com>
2025-08-11 02:25:12 +0300

    Refactor database connection to use `PASSWORD` from environment variable

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,10 @@ use std::error::Error;
 use postgres::{Client, NoTls};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    print!("{}", env::var("PASSWORD")?);
-    let mut client = Client::connect(
-        "host=tf-2025081018303032650000000a.cluster-c1y2k8e8uxdb.eu-central-1.rds.amazonaws.com user=postgres password=wV[P(.QW]4S_iAkT)1E8X(W<J.3Q dbname=db", NoTls)?;
+    let mut client = Client::connect(&format!(
+        "host=tf-2025081018303032650000000a.cluster-c1y2k8e8uxdb.eu-central-1.rds.amazonaws.com user=postgres password={} dbname=db",
+        env::var("PASSWORD")?
+    ), NoTls)?;
 
     for row in client.query("SELECT id, url, sitemap FROM websites", &[])? {
         let id: i16 = row.get(0);
